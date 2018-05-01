4d0854fef11f032c843d21c24610dfde1ef2cd32
npatel0620 <npatel0620@gmail.com>
2018-05-01 13:14:43 -0500

    Add project files.

---
+++ CountryClubProject/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CountryClubProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
